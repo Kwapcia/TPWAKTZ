8c0e3b5ad5edb8591e732b4f5374d201daeda98e
Kwapcia <ola002@outlook.com>
2023-05-13 23:46:25 +0200

    LogicCreateTest etap 2

---
+++ LogicTest/LogicCreateTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace LogicTest
+{
+
+    [TestClass]
+    public class CreationTest
+    {
+        private LogicAbstractApi Api;
+
+        [TestMethod]
+        public void testCreateApi()
+        {
+            Api = LogicAbstractApi.createApi(800, 600);
+            Assert.IsNotNull(Api);
+        }
+
+        [TestMethod]
+        public void getCount()
+        {
+            Api = LogicAbstractApi.createApi(800, 600);
+            Assert.AreEqual(800, Api.width);
+            Assert.AreEqual(600, Api.height);
+            Api.createBalls(5);
+            Assert.AreEqual(5, Api.getAmount);
+            Api.createBalls(-3);
+            Assert.AreEqual(2, Api.getAmount);
+            Api.createBalls(-3);
+            Assert.AreEqual(0, Api.getAmount);
+        }
+    }
+}
