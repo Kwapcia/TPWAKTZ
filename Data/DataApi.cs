5123c015eae465a4abc59b52c7eb29addd6b409b
Kwapcia <ola002@outlook.com>
2023-05-13 22:22:11 +0200

    DataApi etap 2

--- Data/DataApi.cs
+++ Data/DataApi.cs
@@ -1,21 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Threading;
 
 namespace Data
 {
-    public abstract class DataApi
+    public abstract class DataAbstractApi
 
     {
-        public abstract double getXPosition();
-        public abstract double getYPosition();
-        public abstract void setXPosition(double x);
-        public abstract void setYPosition(double y);
-        public static DataApi CreateBall(float X, float Y)
+        public abstract int getAmount { get; }
+        public abstract IList createBallsList(int count);
+        public abstract int width { get; }
+        public abstract int height { get; }
+        public abstract IBall getBall (int index);
+
+        public static DataAbstractApi createApi(int width, int height)
         {
-            return new Ball(X, Y);
+            return new DataApi(width,height);
+        }
+    }
+    internal class DataApi : DataAbstractApi
+    {
+        private ObservableCollection<IBall> balls { get; }
+        private readonly Mutex mutex = new Mutex();
+        private readonly Random random = new Random();
+        public override int width { get; }
+        public override int height { get; }
+
+        public DataApi(int width, int height)
+        {
+            balls = new ObservableCollection<IBall>();
+            this.width = width;
+            this.height = height;
+        }
+
+        public ObservableCollection<IBall> Balls => balls;
+
+        public override IList createBallsList(int count)
+        {
+            if(count > 0)
+            {
+                int ballsCount = balls.Count;
+                for(int i=0;i<count;i++)
+                {
+                    mutex.WaitOne();
+                    int r = 20;
+                    int pom = random.Next(20, 40);
+                    double weight = pom;
+                    double x = random.Next(r, width - r);
+                    double y = random.Next(r, height - r);
+                    double newX = random.Next(- 10, 10) + random.NextDouble();
+                    double newY = random.Next(-10,10)+ random.NextDouble();
+                    Ball ball = new Ball(i + 1 + ballsCount, r, x, y, newX, newY, weight);
+
+                    balls.Add(ball);
+                    mutex.ReleaseMutex();
+
+                }
+            }
+            if(count < 0)
+            {
+                for (int i= count;i<0;i++)
+                {
+                    if(balls.Count > 0)
+                    {
+                        mutex.WaitOne();
+                        balls.Remove(balls[balls.Count-1]); 
+                        mutex.ReleaseMutex();
+                    };
+                }
+            }
+            return balls;
+
+        }
+        public override int getAmount { get => balls.Count; }
+
+        public override IBall getBall(int index)
+        {
+            return balls[index];
         }
     }
 }
