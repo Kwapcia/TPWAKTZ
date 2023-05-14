bcc0ac7e985bceb484e0fbd3eaa9cb20cef4a831
hare32 <234694@edu.p.lodz.pl>
2023-05-14 13:28:18 +0200

    Zmiany w viewModel - MainViewModel

--- Model/ModelAbstractApi.cs
+++ Model/ModelAbstractApi.cs
@@ -1,10 +1,6 @@
 ﻿using Logic;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Model
 {
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,155 @@
+﻿using Model;
+using System.Collections;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class MainWindowViewModel : VM
+    {
+        private readonly ModelAbstractApi modelLayer;
+        private int _BallVal = 1;
+        private int width;
+        private int height;
+        private bool _isStopEnabled = false;
+        private bool isStartEnabled = false;
+        private bool _isAddEnabled = true;
+        private int size = 0;
+        private IList _balls;
+
+        public ICommand addCommand { get; set; }
+
+        public ICommand runCommand { get; set; }
+
+        public ICommand stopCommand
+        { get; set; }
+
+        public MainWindowViewModel()
+        {
+            width = 600;
+            height = 480;
+            modelLayer = ModelAbstractApi.createApi(width, height);
+            stopCommand = new Commands(Stop);
+            addCommand = new Commands(AddBalls);
+            runCommand = new Commands(Start);
+        }
+
+        public bool isStopEnabled
+        {
+            get { return _isStopEnabled; }
+            set
+            {
+                _isStopEnabled = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool isRunEnabled
+        {
+            get { return isStartEnabled; }
+            set
+            {
+                isStartEnabled = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool isAddEnabled
+        {
+            get
+            {
+                return _isAddEnabled;
+            }
+            set
+            {
+                _isAddEnabled = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int ballValue
+        {
+            get
+            {
+                return _BallVal;
+            }
+            set
+            {
+                _BallVal = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                width = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private void AddBalls()
+        {
+            size += ballValue;
+            if (size > 0)
+            {
+                isRunEnabled = true;
+            }
+            else
+            {
+                size = 0;
+                isRunEnabled = false;
+            }
+            Balls = modelLayer.start(ballValue);
+            ballValue = 1;
+        }
+
+        private void Stop()
+        {
+            isStopEnabled = false;
+            isAddEnabled = true;
+            isRunEnabled = true;
+            modelLayer.stop();
+        }
+
+        private void Start()
+        {
+            isStopEnabled = true;
+            isRunEnabled = false;
+            isAddEnabled = false;
+            modelLayer.startMoving();
+        }
+
+        public IList Balls
+        {
+            get => _balls;
+            set
+            {
+                if (value.Equals(_balls))
+                {
+                    return;
+                }
+
+                _balls = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
