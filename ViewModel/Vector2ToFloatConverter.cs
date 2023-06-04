4bbd0cf948639bcf0b726b88c5725d3065449dc7
hare32 <234694@edu.p.lodz.pl>
2023-06-04 12:11:35 +0200

    dodanie converter

---
+++ ViewModel/Vector2ToFloatConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Numerics;
+
+namespace ViewModel
+{
+    public class Vector2ToFloatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Vector2 vector && parameter is string axis)
+            {
+                switch (axis.ToUpper())
+                {
+                    case "X":
+                        return vector.X;
+                    case "Y":
+                        return vector.Y;
+                    default:
+                        throw new InvalidOperationException($"Unsupported conversion axis: {axis}");
+                }
+            }
+
+            throw new InvalidOperationException($"Unsupported conversion type: {value?.GetType()}");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException($"Conversion back is not supported");
+        }
+    }
+}
